8a1e7a677da65fee983aaf86d41bd2ae6a9fac68
Scala Steward <me@scala-steward.org>
2020-03-07 04:32:52 +0100

    Update kamon-core to 2.0.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
 
     val akka = "2.6.3"
     val circe = "0.13.0"
-    val kamon = "1.1.6"
+    val kamon = "2.0.5"
     val scalatest = "3.0.8"
     val akkaHttp = "10.1.11"
     val monix = "3.1.0"
