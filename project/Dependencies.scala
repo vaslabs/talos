9688eda57893dc37825ecbb48215a37ccafc1180
Scala Steward <me@scala-steward.org>
2020-07-22 19:18:31 +0200

    Update kamon-bundle, kamon-core, ... to 2.1.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
 
     val akka = "2.6.8"
     val circe = "0.13.0"
-    val kamon = "2.1.3"
+    val kamon = "2.1.4"
     val scalatest = "3.0.8"
     val akkaHttp = "10.1.12"
     val monix = "3.2.2"
