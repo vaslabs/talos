0b6ad6c6cbd5969a0ac7620626aae98e6d184637
Scala Steward <me@scala-steward.org>
2020-12-09 11:35:16 +0100

    Update akka-http, akka-http-testkit to 10.2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     val circe = "0.13.0"
     val kamon = "2.1.8"
     val scalatest = "3.0.8"
-    val akkaHttp = "10.2.1"
+    val akkaHttp = "10.2.2"
     val monix = "3.2.2"
     val gatling = "3.1.0"
     val wiremock = "2.27.2"
